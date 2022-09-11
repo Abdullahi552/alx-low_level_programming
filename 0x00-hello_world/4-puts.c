35067361831a5053bf0f6f97c26178d9d3503ce2
abdullahi <abdullahiabdulrasheed552@gmail.com>
2022-09-11 09:49:34 +0000

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
 /**
-* main - Betty
-*  
-* Return: 0  after printing the function
-*/
-int main(void) 
+ * main - Betty
+ *
+ * Return: 0  after printing the function
+ */
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0); 
+	return (0);
 }
+
